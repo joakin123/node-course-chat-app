1e31ecc1d13a9851677988d84f43f4d1e57eaa2b
joakin <akinbodejoshua145@gmail.com>
2021-08-30 02:54:45 +0100

    Little modification index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,10 @@
 var socket = io();
 
-socket.on('connect', function () {
+socket.on('connect', () => {
     console.log('connected to server');
 });
 
-socket.on('disconnect', function () {
+socket.on('disconnect', () => {
     console.log('Disconnected from server');
 });
 
